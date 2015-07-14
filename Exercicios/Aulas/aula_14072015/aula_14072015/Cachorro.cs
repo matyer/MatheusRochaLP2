7dbc70356976e6b0c9ebf3d2c37ff2156db9a67d
Matheus Rocha <matheusdarochadias@gmail.com>
2015-07-14 17:47:23 -0300

    Adicionado parte do exercicio de 14/07/2015

---
+++ Exercicios/Aulas/aula_14072015/aula_14072015/Cachorro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_14072015
+{
+    class Cachorro
+    {
+        private int _tamanhoCm; 
+        private string _nome;
+        private int _peso;
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public void SetPeso(int peso)
+        {
+            _peso = peso;
+        }
+
+        public void SetTamanhoCm(int tamanho)
+        {
+            _tamanhoCm = tamanho;
+        }
+
+        public int GetRacaoDiariaKg(int RacaoDiariaKg)
+        {
+            RacaoDiariaKg = _peso / (_tamanhoCm * _tamanhoCm) / 10;
+            return RacaoDiariaKg;
+        }
+
+        public int Getpeso()
+        {
+            return _peso;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public int GetTamanhoCM()
+        {
+            return _tamanhoCm;
+        }
+    }
+}
---
+++ Exercicios/Aulas/aula_14072015/aula_14072015/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_14072015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro fido = new Cachorro();
+            fido.SetTamanhoCm = 95;
+            fido.SetNome = ("Fido");
+            fido.SetPeso(42.3);
+
+            Console.WriteLine("Nome: {0}", fido.GetNome());
+            Console.WriteLine("Peso: {0}", fido.Getpeso());
+            Console.WriteLine("Tamanho : {0}", fido.GetTamanhoCM());
+            Console.WriteLine("Quantidade diária de ração: {0}", fido.GetRacaoDiariaKg());
+        }
+    }
+}
