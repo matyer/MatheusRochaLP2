f7d3deb9f2acccd4973b9fce020b880b64f250a2
luccasz <luccasz555@gmail.com>
2015-09-15 17:51:53 -0300

    Adicionado exercicio 12

---
+++ Terceira Certificacao/exercicio_12/exercicio_12/Gerente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_12
+{
+    class Gerente:Trabalhador
+    {
+        public string Area{ get; set; }
+        public double Adicional { get; set; }
+        public double SalarioTotal
+        {
+            get
+            {
+                return Salario + Adicional;
+            }
+        }
+
+    }
+}
---
+++ Terceira Certificacao/exercicio_12/exercicio_12/Trabalhador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_12
+{
+    class Trabalhador
+    {
+        public string Nome {get; set; }
+        public string Cpf { get; set; }
+        public int DataNascimento { get; set; }
+        public double Salario { get; set; }
+        public int Matricula{ get; set; }
+        
+
+    }
+}
