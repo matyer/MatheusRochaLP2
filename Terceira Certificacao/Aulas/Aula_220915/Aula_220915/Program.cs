f3c399904debf7f0994244ae55261ca3e68b08f5
Matheus Rocha <matheus_msn-55@hotmail.com>
2015-09-22 17:56:36 -0300

    Adicionado aula 22/09 e arrumada pastas 3 certificacao

---
+++ Terceira Certificacao/Aulas/Aula_220915/Aula_220915/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Aula_220915
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strconn = "Server=localhost;Database=test;Uid=root;Pwd=";
+
+            string nome = Console.ReadLine();
+
+            MySqlConnection conn = new MySqlConnection(strconn);
+
+            MySqlCommand command = new MySqlCommand();
+
+            command.Connection = conn;
+
+            command.CommandText = string.Format("INSERT INTO PESSOA (Nome) VALUES ('{0}')", nome);
+
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+    }
+}
